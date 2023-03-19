bd390b4b404dd4beedc9126075bae99b3837314c
ecsree <sreejaec@gmail.com>
2023-03-18 22:45:16 -0500

    second devolop

---
+++ src/test/java/testSuite1/Module2 - Copy.java
@@ -0,0 +1,19 @@
+package testSuite1;
+
+import org.testng.annotations.Test;
+
+public class Module2 {
+	@Test
+	public void test001() {
+		System.out.println("I am executing test case0011");
+	}
+	@Test
+	public void test002() {
+		System.out.println("I am executing test case0021");
+	}
+	@Test
+	public void test003() {
+		System.out.println("I am executing test case0031");
+	}
+
+}
